c1f1e6caed4a5a311d266a077351129edc12380a
Martin Langenbacher <martin.langenbacher@telekom.de>
2020-05-20 10:23:10 +0200

    first commit

---
+++ src/test/java/checkspringpackage/MyStuffBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package checkspringpackage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStuffBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
