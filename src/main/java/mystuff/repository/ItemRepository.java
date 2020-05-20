7d6d274b48b0a366eaa64b3990216cadbf0fecea
Martin Langenbacher <martin.langenbacher@telekom.de>
2020-05-20 15:40:04 +0200

    Zwischenstand, jetzt läuft es...

--- src/main/java/mystuff/repository/ItemRepository.java
+++ src/main/java/mystuff/repository/ItemRepository.java
@@ -15,58 +15,14 @@ import mystuff.entities.Item;
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 	
-	public List<Item> findByAuthor(String author);
-	public Optional<Item> findById(Long id);
+	//public List<Item> findById(String items);
+	//public Optional<Item> findById(Long id);
 	
 	
 	
 
 }
 
-
-
-
-
-
-
-
-
-/*
-package checkspringpackage.repositories;
-
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import checkspringpackage.entities.Book;
-
-
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
-	
-	public Optional<Book> findByTitle(String title);
-	public List<Book> findByAuthor(String author);
-
-}
-
-
-/*
-
-Anmerkung:
-================================================	
-1) Repository vergessen...
-2) FindBy... funktioniert nicht...!
-3) bookRepository.save(book); --> bookRepository unterstrichen: Funktioniert nicht...!
-
-4) bookfound (?) in Eclipse...
-
-
-
-
-*/
 
 
 
