c1977a5317e66e2893b6902ba7e2baf158ce6b1c
Martin Langenbacher <martin.langenbacher@telekom.de>
2020-05-22 10:22:02 +0200

    CRUD klappt schon

--- src/main/java/mystuff/controller/ItemRestController.java
+++ src/main/java/mystuff/controller/ItemRestController.java
@@ -49,56 +49,38 @@ public class ItemRestController {
 	
 	
 	
-	
-	
-	
-	
-	
-	/*
-	
 	@GetMapping("items/{id}")
 	public Optional<Item> get(@PathVariable Long id) {
 		return repository.findById(id);
 		//return ((Object) repository.findById(id)).orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	*/
 	
 	
-	/*
 	
-	@GetMapping("items/{id}")
-	public Item get(@PathVariable Long id) {
-		return repository.findById(id);
-		//return ((Object) repository.findById(id)).orElseThrow(() -> new ResourceNotFoundException(id));
-	}
-	
-	*/
-	
-	
-	/*
 	
 	@PostMapping("items")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Item newItem(@RequestBody Item newItem) {
 		return repository.save(newItem);
 	}
-*/
+
 	
 	
-	/*
 	@PutMapping("items/{id}")
 	public Item replace(@RequestBody Item newItem, @PathVariable Long id) {
-		return repository.findById(id).map(item -> {
-			item.setItemName(newItem.getName());
-			item.setItemAmount(newItem.getAmount());
-			item.setItemLocation(newItem.getLocation());
-			item.setItemDescription(newItem.getDescription());
-			item.setItemDate(newItem.getItemDate());
-			return repository.save(note);
-		});
-		// }).orElseThrow(() -> new ResourceNotFoundException(id));
-		
+		return (Item) repository.findById(id).map(item -> {
+			item.setName(newItem.getName());
+			item.setAmount(newItem.getAmount());
+			item.setLocation(newItem.getLocation());
+			item.setDescription(newItem.getDescription());
+			item.setLastUsed(newItem.getLastUsed());
+			return repository.save(item);
+			
+		}).orElseThrow(() -> new ResourceNotFoundException(id));
 	}
+	
+	
+	
 	
 	
 	@DeleteMapping("items/{id}")
@@ -110,67 +92,8 @@ public class ItemRestController {
 		}
 	}
 
-	*/
+	
 	
 	
 }
-
-
-//========================================================================================
-
-
-/*
-@RestController
-@RequestMapping("/api/v1/")
-public class NotesRestController {
-
-	private final NotesRepository repository;
-
-        @Autowired
-	public NotesRestController(NotesRepository notesRepository) {
-		this.repository = notesRepository;
-	}
-
-	@GetMapping("notes")
-	public List<Notes> getAll() {
-		return repository.findAll();
-	}
-
-	@GetMapping("notes/{id}")
-	public Notes get(@PathVariable Long id) {
-		return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
-	}
-
-	@PostMapping("notes")
-	public Notes create(@RequestBody Notes newNote) {
-		return repository.save(newNote);
-	}
-
-	@PutMapping("notes/{id}")
-	public Notes replace(@RequestBody Notes newNote, @PathVariable Long id) {
-		return repository.findById(id).map(note -> {
-			note.setNotizenname(newNote.getNotizenname());
-			note.setNotizentext(newNote.getNotizentext());
-			return repository.save(note);
-		}).orElseThrow(() -> new ResourceNotFoundException(id));
-	}
-
-	@DeleteMapping("notes/{id}")
-	public void delete(@PathVariable Long id) {
-		try {
-			repository.deleteById(id);
-		} catch (EmptyResultDataAccessException e) {
-			throw new ResourceNotFoundException(id);
-		}
-	}
-
-}
-*/
-
-
-
-
-
-
-
 
