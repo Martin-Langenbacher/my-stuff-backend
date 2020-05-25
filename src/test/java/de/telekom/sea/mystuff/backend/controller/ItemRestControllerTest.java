bf03f75568aea437441df554c293d48c72aa12df
Martin Langenbacher <martin.langenbacher@telekom.de>
2020-05-25 22:00:43 +0200

    Partly working

--- src/test/java/de/telekom/sea/mystuff/backend/controller/ItemRestControllerTest.java
+++ src/test/java/de/telekom/sea/mystuff/backend/controller/ItemRestControllerTest.java
@@ -17,13 +17,19 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import de.telekom.sea.mystuff.backend.entities.Item;
 import de.telekom.sea.mystuff.backend.repository.ItemRepository;
-import io.swagger.models.HttpMethod;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+//import io.swagger.models.HttpMethod;
+import org.springframework.http.HttpMethod;
+
 
 
 
@@ -54,6 +60,20 @@ class ItemRestControllerTest {
 	}
 	
 
+	
+	
+	
+	
+
+
+	
+	
+	
+	
+	
+	
+	
+	
 
 	@Test
 	void shouldBeAbleToUploadAnItem() {
@@ -80,6 +100,22 @@ class ItemRestControllerTest {
 	}
 	
 	
+
+	@Test
+	void shouldReadAllItems() {
+		// Given  |  Arrange
+		Item lawnMower = giveAnInsertedItem().getBody();
+		Item lawnTrimmer = giveAnotherInsertedItem().getBody();
+		// When  |  Act
+		ResponseEntity<Item[]> response = restTemplate.getForEntity(BASE_PATH, Item[].class);  // Achtung: Item ist ein Array - also auch hinten die Item[].class !!!
+		// Then | Assert
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody()).hasSize(2);
+		assertThat(response.getBody()[0]).isEqualToComparingFieldByField(lawnMower);
+		assertThat(response.getBody()[1]).isEqualToComparingFieldByField(lawnTrimmer);
+	}
+	
+	
 	
 	@Test
 	void shouldFindNoItemForUnknownId() throws URISyntaxException {
@@ -91,46 +127,93 @@ class ItemRestControllerTest {
 	}
 	
 	
-	/*
+	
 	@Test
 	void shouldBeAbleToDeleteAnItem() throws URISyntaxException {
 		// Given  |  Arrange
 		Item lawnMower = giveAnInsertedItem().getBody();
 		// When  |  Act
-		RequestEntity<String> requestEntity = new RequestEntity<>(HttpMethod.DELETE, 
-				new URI(restTemplate.getRootUri() + BASE_PATH + "/" +lawnMower.getId()));
+		URI uri = new URI(restTemplate.getRootUri() + BASE_PATH + "/" + lawnMower.getId());
+		RequestEntity<String> requestEntity = new RequestEntity<>(HttpMethod.DELETE, uri);  // RequestEntity<String> requestEntity = new RequestEntity<>(HttpMethod.DELETE, new URI(restTemplate.getRootUri() + BASE_PATH + "/" +lawnMower.getId()));
 		ResponseEntity<String> deleteResponse = restTemplate.exchange(requestEntity, String.class);
 		// Then | Assert
 		assertThat(deleteResponse.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
-		ResponseEntity<Item> getResponse = restTemplate.getForEntity(BASE_PATH + "/" + lawnMower.getId(),  Item.class);
+		ResponseEntity<Item> getResponse = restTemplate.getForEntity(uri, Item.class);
 		assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 	}
-	
-	*/
-	
+		
 	
 	
-	
-	
-	/*
 	@Test
-	void shouldReadAllItems() {
+	void shouldNotBeAbleToDeleteAnItemWithUnknownId() throws URISyntaxException {
 		// Given  |  Arrange
-		Item lawnMower = giveAnInsertedItem().getBody();
+		Long unknownId = 479947l;
 		// When  |  Act
-		//ResponseEntity<Item> response = restTemplate.getForEntity(BASE_PATH, Item.class);
-		ResponseEntity<Item[]> response = restTemplate.getForEntity(BASE_PATH, Item.class);
+		URI uri = new URI(restTemplate.getRootUri() + BASE_PATH + "/" + unknownId);
+		RequestEntity<String> requestEntity = new RequestEntity<>(HttpMethod.DELETE, uri);
+		ResponseEntity<String> deleteResponse = restTemplate.exchange(requestEntity, String.class);
 		// Then | Assert
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-		assertThat(response.getBody()).hasSize(1);
-		assertThat(response.getBody()[0]).isEqualToComparingFieldByField(lawnMower);
+		assertThat(deleteResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 	}
 	
-	*/
+	
+	
+	
+	// TODO / not clear ...!!!
+	
+	// 1) shouldBeAbleToDeleteAnItem  ===================================>> NOT completelly green !!!
+	// 2) shouldBeAbleToReplaceAnItem
+	// 3) shouldNotBeAbleToReplaceAnItemWithUnknownId
+	
+	
+
+	
+	
+
+	
+	private ResponseEntity<Item> giveAnInsertedItem() {
+		Item item = buildLawnMower();
+		return restTemplate.postForEntity(BASE_PATH, item, Item.class);
+	}
+
+	
+	
+	private ResponseEntity<Item> giveAnotherInsertedItem() {
+		Item item = buildLawnTrimmer();
+		return restTemplate.postForEntity(BASE_PATH, item, Item.class);
+	}
+	
+	
+	
+	private Item buildLawnMower() {
+		Item item5 = new Item("Lawn mower", 1, "2019-05-01", "Basement", "Not any comment...");
+		return item5;
+	}
+
+	
+	
+	private Item buildLawnTrimmer() {
+		Item item9 = Item.builder().name("Lawn trimmer").amount(1).lastUsed(LocalDate.parse("2018-05-01"))  // changed: Date.valueOf("2018-05-01")
+				.location("Basement").build();
+		return item9;
+	}
+	
+	
+	
+	// Given  |  Arrange
+	
+	// When  |  Act
+			
+	// Then | Assert
 	
 	
 	
 	
+	
+	
+	
+	
+	/* ====================> ALT / Beispiel - to delete !!!!!!! <===================================
 	
 	@LocalServerPort
 	int randomServerPort;
@@ -145,79 +228,9 @@ class ItemRestControllerTest {
 			System.out.println(item);
 			
 		}
-	}
-	
-	
-	
-	
-	
-	
-	
-	/*
-	
-	@Test
-	void shouldNotBeAbleToDeleteAnItemWithUnknownId() throws URISyntaxException {
-		fail();
-	}
-
-	@Test
-	void shouldBeAbleToReplaceAnItem() throws URISyntaxException {
-		fail();
-	}
-
-	@Test
-	void shouldNotBeAbleToReplaceAnItemWithUnknownId() throws URISyntaxException {
-		fail();
-	}
-
-	
-	
-	*/
-	
-	
-	
-
-	
-	
-
-	private ResponseEntity<Item> giveAnInsertedItem() {
-		Item item = buildLawnMower();
-		return restTemplate.postForEntity(BASE_PATH, item, Item.class);
-	}
-
-	
-	private Item buildLawnMower() {
-		Item item5 = new Item("Lawn mower", 1, "2019-05-01", "Basement", "Not any comment...");
-		//Item item = Item.builder().name("Lawn mower").amount(1).lastUsed("2019-05-01") // change: --> Date.valueOf("2019-05-01")
-		//		.location("Basement").build();
-		return item5;
-	}
-
-	private Item buildLawnTrimmer() {
-		//Item item9 = new Item("Lawn trimmer", 1, "2018-05-01", "Basement", "Not any comment...");
-		Item item9 = Item.builder().name("Lawn trimmer").amount(1).lastUsed(LocalDate.parse("2018-05-01"))  // changed: Date.valueOf("2018-05-01")
-				.location("Basement").build();
-		return item9;
-	}
-	
-	// Item item1 = new Item("Computer", 2, "2020-05-22", "Büro", "Diesen Computer benutze ich im HomeOffice.");
-	
-	
-	
-	
-	
-	// Given  |  Arrange
-	
-		// When  |  Act
-				
-		// Then | Assert
+	} */
 				
 }
 
 	
-	
-	
-	
-
-
 
